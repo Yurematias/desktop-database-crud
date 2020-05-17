8a021dc75d48df5e8af75aa923ccbb634b9a846d
Yurematias <yurematias@ufrn.edu.br>
2020-05-16 22:16:05 -0300

    ipc comunicators added

--- src/desktop/index.js
+++ src/desktop/index.js
@@ -1,5 +1,18 @@
 const { ipcMain } = require('electron');
+const UsersCommunicator = require('./ipc/UsersCommunicator');
+const UserMusicsCommunicator = require('./ipc/UserMusicsCommunicator');
+const MusicsCommunicator = require('./ipc/MusicsCommunicator');
 
 (function main() {
-    ipcMain.on('');
+    ipcMain.on('create-user', UsersCommunicator.create);
+    ipcMain.on('search-user', UsersCommunicator.search);
+    ipcMain.on('list-users', UsersCommunicator.list);
+
+    ipcMain.on('create-music', MusicsCommunicator.create);
+    ipcMain.on('search-music', MusicsCommunicator.search);
+    ipcMain.on('list-musics', MusicsCommunicator.list);
+    
+    ipcMain.on('create-user-music', UserMusicsCommunicator.create);
+    ipcMain.on('search-user-music', UserMusicsCommunicator.search);
+    ipcMain.on('list-user-musics', UserMusicsCommunicator.list);
 })();
