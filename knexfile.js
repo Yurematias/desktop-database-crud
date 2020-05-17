ec6e1c9f4feb388935ff3d209506168d5be0570c
Yurematias <yurematias@ufrn.edu.br>
2020-05-17 01:05:28 -0300

    knexfile.js added

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+module.exports = {
+	development: {
+		client: 'mysql',
+		connection: {
+			host : '127.0.0.1',
+			user : 'Yure',
+			password : '26052000',
+			database : 'lyric_book'
+		},
+		migrations: {
+			directory: './src/desktop/persistance/migrations'
+		}
+	},
+	staging: {
+		client: 'mysql',
+		connection: {
+			host : '127.0.0.1',
+			user : 'Yure',
+			password : '26052000',
+			database : 'lyric_book'
+		},
+		migrations: {
+			directory: './src/desktop/persistance/migrations'
+		}
+	},
+	production: {
+		client: 'mysql',
+		connection: {
+			host : '127.0.0.1',
+			user : 'Yure',
+			password : '26052000',
+			database : 'lyric_book'
+		},
+		migrations: {
+			directory: './src/desktop/persistance/migrations'
+		}
+	}
+};
