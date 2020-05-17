24e92bbfa40abdae87a5aad63d3bb55825e6d5c9
Yurematias <yurematias@ufrn.edu.br>
2020-05-17 01:19:11 -0300

    migrations updated

--- src/desktop/persistance/migrations/20200517011325_create_musics.js
+++ src/desktop/persistance/migrations/20200517011325_create_musics.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('musics', table => {
+        table.string('id').primary();
+        table.string('name').notNullable();
+        table.string('artist').notNullable();
+        table.string('lyrics').notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('musics');
 };
