b7e214026c8cb9d89999fb5a4501eeaf1741825d
Yurematias <yurematias@ufrn.edu.br>
2020-05-19 22:17:19 -0300

    added import of SweetAlerts

--- src/vue/utils/alert.js
+++ src/vue/utils/alert.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+Vue.use(VueSweetalert2, {
+	confirmButtonColor: '#107c10',
+	cancelButtonColor: '#c33217',
+});
 
 export default new Vue({
     methods: {
