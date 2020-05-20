49b54a9ec5075e6af638b98b1444bbb34ceb7250
Yurematias <yurematias@ufrn.edu.br>
2020-05-19 22:18:05 -0300

    import of SweetAlert removed

--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
 import Vue from 'vue'
 import App from './vue/App.vue'
-
 import './vue/global.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faMusic, faHeadphonesAlt, faFileAudio } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
 
 library.add(faUser, faMusic, faHeadphonesAlt, faFileAudio);
  
 Vue.component('FontAwesomeIcon', FontAwesomeIcon);
-Vue.use(VueSweetalert2, {
-	confirmButtonColor: '#107c10',
-	cancelButtonColor: '#c33217',
-});
+
 Vue.config.productionTip = false;
 
 new Vue({
