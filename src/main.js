46dd8c5efbf497b0765901b05267ee097549103a
Yurematias <yurematias@ufrn.edu.br>
2020-05-19 22:20:46 -0300

    FontAwesome import removed

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import Vue from 'vue'
 import App from './vue/App.vue'
 import './vue/global.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUser, faMusic, faHeadphonesAlt, faFileAudio } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(faUser, faMusic, faHeadphonesAlt, faFileAudio);
  
-Vue.component('FontAwesomeIcon', FontAwesomeIcon);
-
 Vue.config.productionTip = false;
 
 new Vue({
