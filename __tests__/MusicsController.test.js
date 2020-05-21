ca4cf3bcb0707ca5a1fb222964271f9a8e7c3d68
Yurematias <yurematias@ufrn.edu.br>
2020-05-21 15:28:15 -0300

    MusicController.test.js removed

--- __tests__/MusicsController.test.js
+++
@@ -1,8 +0,0 @@
-const MusicsController = require('../src/desktop/controllers/MusicsController');
-const connection = require('../src/desktop/persistance/connection');
-
-mController = new MusicsController(connection);
-
-test('', () => {
-    
-});
