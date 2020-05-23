7c7bcd285c378c1cec82864b9c6319ca7127a8bb
Yurematias <yurematias@ufrn.edu.br>
2020-05-23 19:24:52 -0300

    truncateString function test updated

--- __tests__/truncateString.test.js
+++ __tests__/truncateString.test.js
@@ -1,5 +1,6 @@
 import truncateString from '../src/vue/utils/truncateString';
-test('test of truncate string ', () => {
+
+test('test of truncateString function', () => {
     expect(truncateString('Yure Matias', 4)).toBe('Yure');
     expect(truncateString('Luffy', 1)).toBe('L');
     expect(truncateString('Roronoa Zoro', 6)).toBe('Rorono');
