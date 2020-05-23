60e66d2a0d3c86e2b0dcc2b4fa46e44882521250
Yurematias <yurematias@ufrn.edu.br>
2020-05-23 15:36:10 -0300

    create and delete method added

--- src/desktop/ipc/UserMusicsCommunicator.js
+++ src/desktop/ipc/UserMusicsCommunicator.js
@@ -1,5 +1,26 @@
+const UserMusicsController = require('../controllers/UserMusicsController');
+const connection = require('../persistance/connection');
+const userMusicsCont = new UserMusicsController(connection);
+
 module.exports = {
-    create() {},
-    search() {},
+    create(evt, userMusic) {
+        userMusicsCont.create(userMusic).then(() => {
+            console.log('music created successfully');
+            evt.reply('user-music-created', 'user music created successfully');
+        }).catch(error => {
+            console.log(error);
+            evt.reply('user-music-not-created', error.toString());
+        });
+    },
+    delete(evt, userMusic) {
+        const {userId, musicId} = userMusic;
+        userMusicsCont.delete(userId, musicId).then(() => {
+            console.log('user music deleted successfully');
+            evt.reply('user-music-deleted', 'user music deleted sucessfully');
+        }).catch(error => {
+            console.log(error);
+            evt.reply('user-music-not-deleted', error.toString());
+        });
+    },
     list() {}
 }
